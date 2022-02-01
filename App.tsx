680ac26fff26dd3ba7f7c313ccd583a1c8b6859a
Chris Reams <chrisreams91@gmail.com>
2022-02-01 00:28:37 -0600

    stash before figuring out what i broke zzz

--- App.tsx
+++ App.tsx
@@ -20,28 +20,28 @@ interface RemoteEvent {
   eventType: EVENT;
 }
 
-const one = require('./assets/DB_D001_C001_4K_SDR_HEVC.mov');
-const two = require('./assets/DB_D001_C005_4K_SDR_HEVC.mov');
+// const one = require('./assets/DB_D001_C001_4K_SDR_HEVC.mov');
+// const two = require('./assets/DB_D001_C005_4K_SDR_HEVC.mov');
 const three = require('./assets/DB_D011_C009_4K_SDR_HEVC.mov');
 const four = require('./assets/HK_B005_C011_4K_SDR_HEVC.mov');
 const five = require('./assets/LA_A005_C009_4K_SDR_HEVC.mov');
-const six = require('./assets/LA_A006_C008_4K_SDR_HEVC.mov');
+// const six = require('./assets/LA_A006_C008_4K_SDR_HEVC.mov');
 const seven = require('./assets/LA_A009_C009_4K_SDR_HEVC.mov');
-const eight = require('./assets/LW_L001_C006_4K_SDR_HEVC.mov');
+// const eight = require('./assets/LW_L001_C006_4K_SDR_HEVC.mov');
 
 const App = (): JSX.Element => {
   const [paused, setPaused] = useState(false);
   const [showOverLay, setShowOverlay] = useState(true);
   const [currentVideo, setCurrentVideo] = useState(0);
   const [videos, setVideos] = useState([
-    one,
-    two,
+    // one,
+    // two,
     three,
     four,
     five,
-    six,
+    // six,
     seven,
-    eight,
+    // eight,
   ]);
   const [videoPlayerInUse, setVideoPlayerInUser] = useState(0);
 
@@ -56,7 +56,7 @@ const App = (): JSX.Element => {
         const newVideo =
           currentVideo === 0 ? videos.length - 1 : currentVideo - 1;
 
-        refs[currentVideo].seek(0);
+        // refs[currentVideo].seek(0);
         setCurrentVideo(newVideo);
         setVideoPlayerInUser(newVideo);
         setPaused(false);
@@ -66,7 +66,7 @@ const App = (): JSX.Element => {
         const newVideo =
           currentVideo === videos.length - 1 ? 0 : currentVideo + 1;
 
-        refs[currentVideo].seek(0);
+        // refs[currentVideo].seek(0);
         setCurrentVideo(newVideo);
         setVideoPlayerInUser(newVideo);
         setPaused(false);
@@ -86,21 +86,25 @@ const App = (): JSX.Element => {
   useTVEventHandler(TVEventHandler);
 
   const onEnd = () => {
-    setCurrentVideo(currentVideo === videos.length - 1 ? 0 : currentVideo + 1);
+    const newVideo = currentVideo === videos.length - 1 ? 0 : currentVideo + 1;
+    setCurrentVideo(newVideo);
+    setVideoPlayerInUser(newVideo);
+    setPaused(false);
   };
 
-  const refs: Video[] = [];
+  // const refs: Video[] = [];
   return (
     <>
       {videos.map((video: any, index: number) => {
         return (
           <Video
             key={index}
-            ref={(ref) => ref && refs.push(ref)}
+            // ref={(ref) => ref && refs.push(ref)}
             rate={2}
             source={video}
             paused={videoPlayerInUse !== index || paused}
             onEnd={onEnd}
+            repeat={true}
             style={videoPlayerInUse === index && styles.fullScreen}
           />
         );
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TvDashboard
+
+Issues:
+
+production builds seem to fail with files above ~350m seriously limiting the video set
